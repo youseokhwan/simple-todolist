984123ce0d1cb5e083b45a225fd22d293abc81c5
youseokhwan <youseokhwan15@gmail.com>
2022-06-24 14:56:46 +0900

    feat: Settings VC, VM 작성

---
+++ Todolist/Todolist/Presentation/Settings/SettingsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsViewController.swift
+//  Todolist
+//
+//  Created by 유석환 on 2022/06/24.
+//
+
+import UIKit
+
+final class SettingsViewController: UIViewController {
+    private let viewModel = SettingsViewModel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
