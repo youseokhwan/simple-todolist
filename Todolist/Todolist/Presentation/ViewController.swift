90065552fad007ba6dca08d6932104a000c27bcd
고재경 <folw159@gmail.com>
2022-06-24 15:34:24 +0900

    feat: Snapkit import 테스트

--- Todolist/Todolist/Presentation/ViewController.swift
+++ Todolist/Todolist/Presentation/ViewController.swift
@@ -9,7 +9,17 @@ import UIKit
 import SnapKit
 
 final class ViewController: UIViewController {
+    private lazy var label = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        label.text = "success"
+        label.textColor = .white
+        
+        view.addSubview(label)
+        label.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(30)
+        }
     }
 }
