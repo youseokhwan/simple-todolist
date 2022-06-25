2da7fa0cd580105a22b4bc6759d0dc7dad60fc81
고재경 <folw159@gmail.com>
2022-06-25 14:09:38 +0900

    feat: TasksViewController에 From, Settings BarButton 생성 및 화면 전환 구현

--- Todolist/Todolist/Presentation/Tasks/TasksViewController.swift
+++ Todolist/Todolist/Presentation/Tasks/TasksViewController.swift
@@ -12,8 +12,45 @@ import RxCocoa
 
 final class TasksViewController: UIViewController {
     private let viewModel = TasksViewModel()
+    private let disposeBag = DisposeBag()
+    
+    private lazy var formButton = UIButton()
+    private lazy var settingsButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
+    }
+    
+    private func configure() {
+        let buttonImageConfiguration = UIImage.SymbolConfiguration(pointSize: 25)
+        formButton.setImage(UIImage(systemName: "plus", withConfiguration: buttonImageConfiguration), for: .normal)
+        settingsButton.setImage(UIImage(systemName: "gearshape.fill", withConfiguration: buttonImageConfiguration), for: .normal)
+        
+        formButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                self?.formButtonTapped()
+            })
+            .disposed(by: disposeBag)
+        
+        settingsButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                self?.formButtonTapped()
+            })
+            .disposed(by: disposeBag)
+        
+        let formBarButton = UIBarButtonItem(customView: formButton)
+        let settingsBarButton = UIBarButtonItem(customView: settingsButton)
+        navigationItem.rightBarButtonItems = [formBarButton, settingsBarButton]
+    }
+    
+    private func formButtonTapped() {
+        let formViewController = FormViewController()
+        navigationController?.pushViewController(formViewController, animated: true)
+    }
+    
+    private func settingsButtonTapped() {
+        let settingsViewController = SettingsViewController()
+        navigationController?.pushViewController(settingsViewController, animated: true)
     }
 }
