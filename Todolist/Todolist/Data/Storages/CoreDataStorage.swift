ed207f93faee41a1c54220763766ba5976478091
youseokhwan <youseokhwan15@gmail.com>
2022-06-28 13:15:37 +0900

    feat: fetchAllTasks() 구현

--- Todolist/Todolist/Data/Storages/CoreDataStorage.swift
+++ Todolist/Todolist/Data/Storages/CoreDataStorage.swift
@@ -5,8 +5,30 @@
 //  Created by 유석환 on 2022/06/27.
 //
 
+import CoreData
 import Foundation
 
 final class CoreDataStorage {
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Model")
+        container.loadPersistentStores(completionHandler: { _, error in
+            guard error == nil else {
+                print(#file, #line, "loadPersistentStores error")
+                return
+            }
+        })
+        return container
+    }()
+    lazy var viewContext = persistentContainer.viewContext
 
+    func fetchAllTasks() -> [CDTask] {
+        let fetchRequest = CDTask.fetchRequest()
+
+        if let cdTasks = try? viewContext.fetch(fetchRequest) {
+            return cdTasks
+        } else {
+            print(#file, #line, "fetch fail")
+            return []
+        }
+    }
 }
