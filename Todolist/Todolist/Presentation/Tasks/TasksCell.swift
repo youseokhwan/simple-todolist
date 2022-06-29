a203b8ade4bab42b3a72840ca529b3aa1ddad596
고재경 <folw159@gmail.com>
2022-06-29 16:20:10 +0900

    feat: TasksCell UI컴포넌트 작성

--- Todolist/Todolist/Presentation/Tasks/TasksCell.swift
+++ Todolist/Todolist/Presentation/Tasks/TasksCell.swift
@@ -6,9 +6,20 @@
 //
 
 import UIKit
+import SnapKit
 
 final class TasksCell: UITableViewCell {
     static let identifier = "TasksCell"
     
-    
+    private lazy var contextLabel = UILabel()
+    private lazy var endDataLabel = UILabel()
+    private lazy var publisedDateLabel = UILabel()
+    private lazy var checkButton: UIButton = {
+        let button = UIButton()
+        let configure = UIImage.SymbolConfiguration(pointSize: 20)
+        button.setImage(UIImage(systemName: "circle", withConfiguration: configure), for: .normal)
+        button.setImage(UIImage(systemName: "checkmark.circle.fill", withConfiguration: configure), for: .selected)
+        button.tintColor = .systemGreen
+        return button
+    }()
 }
