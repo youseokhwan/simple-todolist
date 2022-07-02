b6c7cbaab196a4d849ba2aa4621899601ceb5dd4
youseokhwan <youseokhwan15@gmail.com>
2022-07-02 23:45:29 +0900

    feat: FormVC UI 컴포넌트 프로퍼티 선언

--- Todolist/Todolist/Presentation/Form/FormViewController.swift
+++ Todolist/Todolist/Presentation/Form/FormViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 final class FormViewController: UIViewController {
     private let viewModel = FormViewModel()
 
+    private lazy var contextTextField: UITextField = {
+        let textField = UITextField()
+        return textField
+    }()
+    private lazy var publishedDateLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    private lazy var publishedDateButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    private lazy var endDateLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    private lazy var endDateButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
