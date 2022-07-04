4c1131d69fd5fcb2575b8682fe2cea8e529d7fb1
youseokhwan <youseokhwan15@gmail.com>
2022-07-04 22:15:53 +0900

    feat: FormDailyView 생성

---
+++ Todolist/Todolist/Presentation/Form/Subviews/FormDailyView.swift
@@ -0,0 +1,24 @@
+//
+//  FormDailyView.swift
+//  Todolist
+//
+//  Created by 유석환 on 2022/07/04.
+//
+
+import UIKit
+
+final class FormDailyView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+
+    private func configure() {
+
+    }
+}
