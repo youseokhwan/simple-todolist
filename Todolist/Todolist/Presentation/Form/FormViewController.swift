aa974f8670911e9180dde756192c9fca83d22e0b
youseokhwan <youseokhwan15@gmail.com>
2022-07-04 18:59:17 +0900

    refactor: UI들을 VC에서 FormStackView로 이동

--- Todolist/Todolist/Presentation/Form/FormViewController.swift
+++ Todolist/Todolist/Presentation/Form/FormViewController.swift
@@ -14,32 +14,6 @@ final class FormViewController: UIViewController {
 
     private lazy var scrollView = UIScrollView()
     private lazy var stackView = FormStackView()
-    
-    // TODO: StackView 분리해야 함
-    private lazy var contextTextField: UITextField = {
-        let textField = UITextField()
-        return textField
-    }()
-    private lazy var publishedDateLabel: UILabel = {
-        let label = UILabel()
-        label.text = "시작일"
-        return label
-    }()
-    private lazy var publishedDateButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("2022.07.03(일)", for: .normal)
-        return button
-    }()
-    private lazy var endDateLabel: UILabel = {
-        let label = UILabel()
-        label.text = "종료일"
-        return label
-    }()
-    private lazy var endDateButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("2022.07.10(일)", for: .normal)
-        return button
-    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -56,12 +30,6 @@ final class FormViewController: UIViewController {
 
         view.addSubview(scrollView)
         scrollView.addSubview(stackView)
-
-        [contextTextField, publishedDateLabel, publishedDateButton, endDateLabel, endDateButton]
-            .forEach {
-                $0.backgroundColor = .systemGray // TODO: UI 작업 후 삭제
-                stackView.addArrangedSubview($0)
-            }
     }
 
     private func configureConstraints() {
--- Todolist/Todolist/Presentation/Form/Subviews/FormStackView.swift
+++ Todolist/Todolist/Presentation/Form/Subviews/FormStackView.swift
@@ -8,6 +8,31 @@
 import UIKit
 
 final class FormStackView: UIStackView {
+    private lazy var contextTextField: UITextField = {
+        let textField = UITextField()
+        return textField
+    }()
+    private lazy var publishedDateLabel: UILabel = {
+        let label = UILabel()
+        label.text = "시작일"
+        return label
+    }()
+    private lazy var publishedDateButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("2022.07.03(일)", for: .normal)
+        return button
+    }()
+    private lazy var endDateLabel: UILabel = {
+        let label = UILabel()
+        label.text = "종료일"
+        return label
+    }()
+    private lazy var endDateButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("2022.07.10(일)", for: .normal)
+        return button
+    }()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
@@ -20,5 +45,11 @@ final class FormStackView: UIStackView {
 
     private func configure() {
         axis = .vertical
+
+        [contextTextField, publishedDateLabel, publishedDateButton, endDateLabel, endDateButton]
+            .forEach {
+                $0.backgroundColor = .systemGray // TODO: UI 작업 후 삭제
+                addArrangedSubview($0)
+            }
     }
 }
