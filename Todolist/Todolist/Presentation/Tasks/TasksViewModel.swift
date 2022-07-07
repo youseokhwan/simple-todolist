4e1c81bfa2b1d93d54648e893491d38c3cb7a15d
youseokhwan <youseokhwan15@gmail.com>
2022-07-07 12:11:59 +0900

    refactor: isDaily, isChecked 순서 변경

--- Todolist/Todolist/Presentation/Tasks/TasksViewModel.swift
+++ Todolist/Todolist/Presentation/Tasks/TasksViewModel.swift
@@ -12,9 +12,9 @@ import RxSwift
 final class TasksViewModel {
     var delegate: TasksViewModelDelegate?
     let tasksObserver: Observable<[Task]> = Observable
-        .just([Task(id: "0", context: "첫번째 할일", isChecked: false, isDaily: false),
-               Task(id: "1", context: "두번째 할일", isChecked: false, isDaily: false),
-               Task(id: "2", context: "세번째 할일", isChecked: false, isDaily: false)])
+        .just([Task(id: "0", context: "첫번째 할일", isDaily: false, isChecked: false),
+               Task(id: "1", context: "두번째 할일", isDaily: false, isChecked: false),
+               Task(id: "2", context: "세번째 할일", isDaily: false, isChecked: false)])
     
     func didTappedFormButton() {
         delegate?.didTappedFormButton()
