d227584b2d81d9198db7c59b3064ae9bcc2e0540
고재경 <folw159@gmail.com>
2022-07-11 15:33:44 +0900

    feat: RxDataSource 구현

--- Todolist/Todolist/Presentation/Settings/SettingsViewController.swift
+++ Todolist/Todolist/Presentation/Settings/SettingsViewController.swift
@@ -7,12 +7,24 @@
 
 import UIKit
 
+import RxCocoa
+import RxDataSources
+import RxSwift
 import SnapKit
 
 final class SettingsViewController: UIViewController {
     private let viewModel = SettingsViewModel()
-    
+    private let disposeBag = DisposeBag()
+
     private lazy var settingsTableView = UITableView()
+        
+    private var tableViewDataSource = RxTableViewSectionedReloadDataSource<SettingsTableViewCellModel>(
+        configureCell: { dataSource, tableView, indexPath, item in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+            var content = cell.defaultContentConfiguration()
+            content.text = item
+            return cell
+        })
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +33,7 @@ final class SettingsViewController: UIViewController {
     
     private func configure() {
         configureViews()
+        configureRx()
         configureConstraints()
     }
     
@@ -31,11 +44,21 @@ final class SettingsViewController: UIViewController {
         navigationItem.title = "설정"
         
         view.addSubview(settingsTableView)
+                
+        tableViewDataSource.titleForHeaderInSection = { dataSource, index in
+            return dataSource.sectionModels[index].headerTitle
+        }
     }
     
     private func configureConstraints() {
         settingsTableView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
+    }
+    
+    private func configureRx() {
+        Observable.just(viewModel.items)
+            .bind(to: settingsTableView.rx.items(dataSource: tableViewDataSource))
+            .disposed(by: disposeBag)
     }
 }
