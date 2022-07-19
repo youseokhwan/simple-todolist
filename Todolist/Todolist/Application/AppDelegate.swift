8aaab1046886b1d8f443ee17d993891f4d54a7e1
고재경 <folw159@gmail.com>
2022-07-19 16:17:23 +0900

    refactor: AppDelegate 컨벤션 통일 작업

--- Todolist/Todolist/Application/AppDelegate.swift
+++ Todolist/Todolist/Application/AppDelegate.swift
@@ -17,6 +17,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      configurationForConnecting connectingSceneSession: UISceneSession,
                      options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        return UISceneConfiguration(name: "Default Configuration",
+                                    sessionRole: connectingSceneSession.role)
     }
 }
