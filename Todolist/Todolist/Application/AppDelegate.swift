a7d66e215387c98c0104639a84b9557a71b9d964
고재경 <folw159@gmail.com>
2022-07-19 16:41:51 +0900

    refactor: AppDelegate 개행

--- Todolist/Todolist/Application/AppDelegate.swift
+++ Todolist/Todolist/Application/AppDelegate.swift
@@ -9,8 +9,10 @@ import UIKit
 
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
         return true
     }
 
