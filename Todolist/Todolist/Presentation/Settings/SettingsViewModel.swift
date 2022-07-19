330f1511b2ab91814b26cb4f006114127fc9d67d
youseokhwan <youseokhwan15@gmail.com>
2022-07-19 15:58:49 +0900

    refactor: SettingsVM init 작성

--- Todolist/Todolist/Presentation/Settings/SettingsViewModel.swift
+++ Todolist/Todolist/Presentation/Settings/SettingsViewModel.swift
@@ -8,14 +8,12 @@
 import Foundation
 
 final class SettingsViewModel {
-    let items = [
-        SettingsSection(
-            title: "설정",
-            items: ["테마 설정"]
-        ),
-        SettingsSection(
-            title: "앱 정보",
-            items: ["오픈소스 라이센스", "버전 정보", "수정사항", "Contact Us"]
-        )
-    ]
+    let items: [SettingsSection]
+
+    init() {
+        items = [SettingsSection(title: "설정",
+                                 items: ["테마 설정"]),
+                 SettingsSection(title: "앱 정보",
+                                 items: ["오픈소스 라이센스", "버전 정보", "수정사항", "Contact Us"])]
+    }
 }
