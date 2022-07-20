cd191ee13ae1a7c97dc36ce2903980eb4ed3e920
고재경 <folw159@gmail.com>
2022-07-20 22:50:37 +0900

    feat: Cell UI컴포넌트 및 생성자 추가

--- Todolist/Todolist/Presentation/Settings/Subviews/ThemeTableViewCell.swift
+++ Todolist/Todolist/Presentation/Settings/Subviews/ThemeTableViewCell.swift
@@ -8,5 +8,17 @@
 import UIKit
 
 final class ThemeTableViewCell: UITableViewCell {
-    
+    private let identifier = "ThemeTableViewCell"
+
+    private lazy var titleLabel = UILabel()
+    private lazy var currentThemeLabel = UILabel()
+    private lazy var button = UIButton()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
