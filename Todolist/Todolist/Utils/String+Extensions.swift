0b97060093011c182f98f81fbf4c6edf4f72ab5f
youseokhwan <youseokhwan15@gmail.com>
2022-07-20 17:57:44 +0900

    feat: localized extension 구현

---
+++ Todolist/Todolist/Utils/String+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  String+Extensions.swift
+//  Todolist
+//
+//  Created by 유석환 on 2022/07/20.
+//
+
+import Foundation
+
+extension String {
+    var localized: String {
+        return NSLocalizedString(self, tableName: "Localizable", value: self, comment: "")
+    }
+}
