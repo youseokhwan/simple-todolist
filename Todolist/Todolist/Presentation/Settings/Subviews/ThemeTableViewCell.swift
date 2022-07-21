ea0dea8ee9efb5e059c50c8c889d1dbaf8edbb7b
고재경 <folw159@gmail.com>
2022-07-21 13:42:00 +0900

    feat: button에 menu 추가

--- Todolist/Todolist/Presentation/Settings/Subviews/ThemeTableViewCell.swift
+++ Todolist/Todolist/Presentation/Settings/Subviews/ThemeTableViewCell.swift
@@ -12,7 +12,28 @@ final class ThemeTableViewCell: UITableViewCell {
 
     private lazy var titleLabel = UILabel()
     private lazy var currentThemeLabel = UILabel()
-    private lazy var button = UIButton()
+    private lazy var button: UIButton = {
+        let button = UIButton()
+        let systemMode = UIAction(title: "시스템") { action in
+            print("system")
+        }
+        let lightMode = UIAction(title: "라이트") { action in
+            print("light")
+        }
+        let darkMode = UIAction(title: "다크") { action in
+            print("dark")
+        }
+        let cancel = UIAction(title: "취소",attributes: .destructive) { _ in
+            print("cancel")
+        }
+
+        button.menu = UIMenu(
+            title: "", options: .displayInline, children: [systemMode, lightMode, darkMode, cancel]
+        )
+        button.showsMenuAsPrimaryAction = true
+
+        return button
+    }()
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
