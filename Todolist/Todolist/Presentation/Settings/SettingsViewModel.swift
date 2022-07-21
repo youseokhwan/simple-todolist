901ab3eca4de2b5fe703debea4e26dca28d8ea00
고재경 <folw159@gmail.com>
2022-07-21 15:12:10 +0900

    fix: SettingsViewModel 수정

--- Todolist/Todolist/Presentation/Settings/SettingsViewModel.swift
+++ Todolist/Todolist/Presentation/Settings/SettingsViewModel.swift
@@ -11,9 +11,16 @@ final class SettingsViewModel {
     let items: [SettingsSection]
 
     init() {
-        items = [SettingsSection(title: "설정",
-                                 items: ["테마 설정"]),
-                 SettingsSection(title: "앱 정보",
-                                 items: ["오픈소스 라이센스", "버전 정보", "수정사항", "Contact Us"])]
+        items = [
+            .ThemeCell(title: "설정", items: [
+                .ThemeItem(title: "테마 설정", currentTheme: "light")
+            ]),
+            .DefaultCell(title: "앱 정보", items: [
+                .DefaultItem(title: "오픈소스 라이센스"),
+                .DefaultItem(title: "버전 정보"),
+                .DefaultItem(title: "수정사항"),
+                .DefaultItem(title: "Contact Us")
+            ])
+        ]
     }
 }
