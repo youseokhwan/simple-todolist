b2a192c93e97b21284b54fe147665907664fa43e
고재경 <folw159@gmail.com>
2022-07-22 17:47:31 +0900

    fix: enum SettingsSection -> struct Settings Section 변경, localizeable Ko ; 추가

--- Todolist/Todolist/Domain/Entities/SettingsSection.swift
+++ Todolist/Todolist/Domain/Entities/SettingsSection.swift
@@ -9,36 +9,14 @@ import Foundation
 
 import RxDataSources
 
-enum SettingsSection {
-    case DefaultCell(title: String, items: [SectionItem])
-    case ThemeCell(title: String, items: [SectionItem])
+struct SettingsSection {
+    var title: String
+    var items: [SectionItem]
 }
 
 extension SettingsSection: SectionModelType {
-    var title: String {
-        switch self {
-        case .DefaultCell(title: let title, items: _):
-            return title
-        case .ThemeCell(title: let title, items: _):
-            return title
-        }
-    }
-
-    var items: [SectionItem] {
-        switch self {
-        case .DefaultCell(title: _, items: let items):
-            return items.map { $0 }
-        case .ThemeCell(title: _, items: let items):
-            return items.map { $0 }
-        }
-    }
-
     init(original: SettingsSection, items: [SectionItem]) {
-        switch original {
-        case let .DefaultCell(title, _):
-            self = .DefaultCell(title: title, items: items)
-        case let .ThemeCell(title, _):
-            self = .ThemeCell(title: title, items: items)
-        }
+        self = original
+        self.items = items
     }
 }
--- Todolist/Todolist/Presentation/Settings/SettingsViewModel.swift
+++ Todolist/Todolist/Presentation/Settings/SettingsViewModel.swift
@@ -11,12 +11,13 @@ final class SettingsViewModel {
     let items: [SettingsSection]
 
     init() {
-        items = [.ThemeCell(title: Const.appSettingsSection,
-                            items: [.Theme(title: Const.themeSettings, currentTheme: Const.lightTheme)]),
-                 .DefaultCell(title: Const.informationSection,
-                              items: [.Default(title: Const.openSourceLicense),
-                                      .Default(title: Const.versionInfomation),
-                                      .Default(title: Const.patchDetails),
-                                      .Default(title: Const.contactUs)])]
+        items = [SettingsSection(title: Const.appSettingsSection,
+                                 items: [.Theme(title: Const.themeSettings,
+                                                currentTheme: Const.lightTheme)]),
+                 SettingsSection(title: Const.informationSection,
+                                 items: [.Default(title: Const.openSourceLicense),
+                                         .Default(title: Const.versionInfomation),
+                                         .Default(title: Const.patchDetails),
+                                         .Default(title: Const.contactUs)])]
     }
 }
