5a884b5458e68da75b9c90b90e3e4f739c6266cb
youseokhwan <youseokhwan15@gmail.com>
2022-07-25 01:57:26 +0900

    feat: UpdateTaskUseCase 구현

---
+++ Todolist/Todolist/Domain/UseCases/UpdateTaskUseCase.swift
@@ -0,0 +1,16 @@
+//
+//  UpdateTaskUseCase.swift
+//  Todolist
+//
+//  Created by 유석환 on 2022/07/25.
+//
+
+import Foundation
+
+struct UpdateTaskUseCase {
+    private let taskRepository = TaskRepository()
+
+    func update(task: Task) {
+        taskRepository.update(task: task)
+    }
+}
