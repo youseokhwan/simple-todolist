dbb48c787042d9451376549f3148e2ef40f8137b
youseokhwan <youseokhwan15@gmail.com>
2022-07-28 22:25:10 +0900

    feat: ThemeAlertController Action 추가

--- Todolist/Todolist/Presentation/Settings/Subviews/ThemeAlertController.swift
+++ Todolist/Todolist/Presentation/Settings/Subviews/ThemeAlertController.swift
@@ -8,5 +8,21 @@
 import UIKit
 
 final class ThemeAlertController: UIAlertController {
+    convenience init(children: [String]) {
+        self.init(title: Const.themeMenuTitle, message: nil, preferredStyle: .actionSheet)
+        configure(children: children)
+    }
+}
 
+private extension ThemeAlertController {
+    func configure(children: [String]) {
+        children.enumerated()
+            .map { index, value in
+                UIAlertAction(title: value, style: .default, handler: nil)
+            }
+            .forEach {
+                addAction($0)
+            }
+        addAction(UIAlertAction(title: Const.cancel, style: .cancel))
+    }
 }
