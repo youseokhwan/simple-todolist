a88e788f2ed4b7fb730d3fc659f8146185c29684
youseokhwan <youseokhwan15@gmail.com>
2022-07-28 21:52:41 +0900

    refactor: available을 로직 안쪽에서 분기하도록 수정

--- Todolist/Todolist/Presentation/Settings/Subviews/ThemeMenuButton.swift
+++ Todolist/Todolist/Presentation/Settings/Subviews/ThemeMenuButton.swift
@@ -7,7 +7,6 @@
 
 import UIKit
 
-@available(iOS 14.0, *)
 final class ThemeMenuButton: UIButton {
     convenience init(handler: @escaping (Int) -> Void) {
         self.init()
@@ -15,26 +14,29 @@ final class ThemeMenuButton: UIButton {
     }
 }
 
-@available(iOS 14.0, *)
 private extension ThemeMenuButton {
     func configure(handler: @escaping (Int) -> Void) {
-        let currentAppearance = UserDefaultsRepository.currentAppearance()
-        let themes = [Const.systemTheme, Const.lightTheme, Const.darkTheme]
-        let children = themes.enumerated().map { index, value in
-            return UIAction(title: value) { [weak self] action in
-                handler(index)
-                self?.setTitle(value, for: .normal)
-                self?.sizeToFit()
+        if #available(iOS 14.0, *) {
+            let currentAppearance = UserDefaultsRepository.currentAppearance()
+            let themes = [Const.systemTheme, Const.lightTheme, Const.darkTheme]
+            let children = themes.enumerated().map { index, value in
+                return UIAction(title: value) { [weak self] action in
+                    handler(index)
+                    self?.setTitle(value, for: .normal)
+                    self?.sizeToFit()
+                }
             }
+
+            menu = UIMenu(title: Const.themeMenuTitle,
+                          options: .displayInline,
+                          children: children)
+            showsMenuAsPrimaryAction = true
+
+            setTitle(themes[currentAppearance], for: .normal)
+            setTitleColor(.systemBlue, for: .normal)
+            sizeToFit()
+        } else {
+
         }
-
-        menu = UIMenu(title: Const.themeMenuTitle,
-                      options: .displayInline,
-                      children: children)
-        showsMenuAsPrimaryAction = true
-
-        setTitle(themes[currentAppearance], for: .normal)
-        setTitleColor(.systemBlue, for: .normal)
-        sizeToFit()
     }
 }
