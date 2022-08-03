fb24d3f110bed77bb1b6f54c94fc128c00f8cb89
youseokhwan <youseokhwan15@gmail.com>
2022-08-03 15:13:01 +0900

    feat: strikethrough(isActive:) 정의

--- Todolist/Todolist/Utils/UILabel+Extensions.swift
+++ Todolist/Todolist/Utils/UILabel+Extensions.swift
@@ -8,5 +8,22 @@
 import UIKit
 
 extension UILabel {
+    func strikethrough(isActive: Bool) {
+        guard let text = text else { return }
 
+        if isActive {
+            let attributedString = NSMutableAttributedString(string: text)
+
+            attributedString.addAttribute(.strikethroughStyle,
+                                          value: NSUnderlineStyle.single.rawValue,
+                                          range: NSMakeRange(0, attributedString.length))
+            attributedString.addAttribute(.foregroundColor,
+                                          value: UIColor.systemGray,
+                                          range: NSMakeRange(0, attributedString.length))
+
+            attributedText = attributedString
+        } else {
+            attributedText = NSAttributedString(string: text)
+        }
+    }
 }
