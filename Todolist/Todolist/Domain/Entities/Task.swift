497aa1cbdad8da9e227e7b540b8122589da75676
youseokhwan <youseokhwan15@gmail.com>
2022-08-04 16:09:02 +0900

    feat: Task, CDTask에 publishedDate 항목 추가

--- Todolist/Todolist/Domain/Entities/Task.swift
+++ Todolist/Todolist/Domain/Entities/Task.swift
@@ -9,12 +9,14 @@ import Foundation
 
 struct Task {
     let id: String
+    var publishedDate: String
     var context: String
     var isDaily: Bool
     var isChecked: Bool
 
-    init(id: String, context: String, isDaily: Bool, isChecked: Bool) {
+    init(id: String, publishedDate: String, context: String, isDaily: Bool, isChecked: Bool) {
         self.id = id
+        self.publishedDate = publishedDate
         self.context = context
         self.isDaily = isDaily
         self.isChecked = isChecked
@@ -25,6 +27,7 @@ struct Task {
               let id = cdTask.id else { return nil }
 
         self.id = id
+        self.publishedDate = cdTask.publishedDate ?? "1900.01.01"
         self.context = cdTask.context ?? ""
         self.isDaily = cdTask.isDaily
         self.isChecked = cdTask.isChecked
