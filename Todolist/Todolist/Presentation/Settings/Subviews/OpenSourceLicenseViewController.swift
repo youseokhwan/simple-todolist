8bb941c56ae1768df415b884dcada33e9ce03005
고재경 <folw159@gmail.com>
2022-08-13 20:58:44 +0900

    feat: OpenSourceLicense VC 생성

---
+++ Todolist/Todolist/Presentation/Settings/Subviews/OpenSourceLicenseViewController.swift
@@ -0,0 +1,12 @@
+//
+//  OpenSourceLicenseViewController.swift
+//  Todolist
+//
+//  Created by Jae Kyeong Ko on 2022/08/13.
+//
+
+import UIKit
+
+class OpenSourceLicenseViewController: UIViewController {
+
+}
