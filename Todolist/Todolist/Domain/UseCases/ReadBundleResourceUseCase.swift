f9837b9ef5e234b178f1753160e779115c4400d7
youseokhwan <youseokhwan15@gmail.com>
2022-08-16 17:44:59 +0900

    feat: licensesText() 구현

--- Todolist/Todolist/Domain/UseCases/ReadBundleResourceUseCase.swift
+++ Todolist/Todolist/Domain/UseCases/ReadBundleResourceUseCase.swift
@@ -8,5 +8,17 @@
 import Foundation
 
 struct ReadBundleResourceUseCase {
+    enum LicenseFileName: String, CaseIterable {
+        case realmSwift = "realm-swift"
+        case rxDataSources = "RxDataSources"
+        case rxRealm = "RxRealm"
+        case rxSwift = "RxSwift"
+        case snapKit = "SnapKit"
+    }
 
+    func licensesText() -> String {
+        let text = LicenseFileName.allCases.map { $0.rawValue }.joined(separator: "\n=====\n")
+
+        return text
+    }
 }
