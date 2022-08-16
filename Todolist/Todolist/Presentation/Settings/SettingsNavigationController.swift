40cac482e8cdd49ac4284b31bf44d431e3a7e48d
고재경 <folw159@gmail.com>
2022-08-16 18:19:54 +0900

    feat: SettingsNavigationController 생성

---
+++ Todolist/Todolist/Presentation/Settings/SettingsNavigationController.swift
@@ -0,0 +1,15 @@
+//
+//  SettingsNavigationController.swift
+//  Todolist
+//
+//  Created by Jae Kyeong Ko on 2022/08/16.
+//
+
+import UIKit
+
+final class SettingsNavigationController: UINavigationController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
