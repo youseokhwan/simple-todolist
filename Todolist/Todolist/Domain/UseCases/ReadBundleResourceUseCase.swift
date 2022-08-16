887867d31fca38aee43e7db070d450e6642656e8
youseokhwan <youseokhwan15@gmail.com>
2022-08-17 01:26:01 +0900

    fix: 불필요한 sort 로직 삭제

--- Todolist/Todolist/Domain/UseCases/ReadBundleResourceUseCase.swift
+++ Todolist/Todolist/Domain/UseCases/ReadBundleResourceUseCase.swift
@@ -8,20 +8,16 @@
 import Foundation
 
 struct ReadBundleResourceUseCase {
-    enum LicenseFileName: String, CaseIterable, Comparable {
+    enum LicenseFileName: String, CaseIterable {
         case realmSwift = "realm-swift"
         case rxDataSources = "RxDataSources"
         case rxRealm = "RxRealm"
         case rxSwift = "RxSwift"
         case snapKit = "SnapKit"
-
-        static func < (lhs: Self, rhs: Self) -> Bool {
-            return lhs.rawValue < rhs.rawValue
-        }
     }
 
     func licensesText() -> String {
-        let text = LicenseFileName.allCases.sorted().map {
+        let text = LicenseFileName.allCases.map {
             guard let fileURL = Bundle.main.url(forResource: $0.rawValue, withExtension: "txt"),
                   let contents = try? String(contentsOf: fileURL) else { return "" }
 
