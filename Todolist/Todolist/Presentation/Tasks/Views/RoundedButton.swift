1bc96e926e8155fa8b5076153772c7568fcf6596
고재경 <folw159@gmail.com>
2022-08-21 16:49:13 +0900

    feat: Rounded Button 생성

---
+++ Todolist/Todolist/Presentation/Tasks/Views/RoundedButton.swift
@@ -0,0 +1,18 @@
+//
+//  RoundedButton.swift
+//  Todolist
+//
+//  Created by Jae Kyeong Ko on 2022/08/21.
+//
+
+import UIKit
+
+final class RoundedButton: UIButton {
+    convenience init() {
+        self.init()
+    }
+}
+
+private extension RoundedButton {
+    
+}
