753741dd881002400d0f1e20b5fe8bdb4bfa3279
youseokhwan <youseokhwan15@gmail.com>
2022-09-10 01:49:19 +0900

    feat: text(_:) extension 메소드 구현

--- Todolist/Todolist/Utils/String+Extensions.swift
+++ Todolist/Todolist/Utils/String+Extensions.swift
@@ -8,6 +8,22 @@
 import Foundation
 
 extension String {
+    static func text(_ key: String) -> String {
+        guard let path = Bundle.main.path(forResource: "Text", ofType: "plist") else { return "" }
+
+        let url = URL(fileURLWithPath: path)
+
+        guard let data = try? Data(contentsOf: url),
+              let plist = try? PropertyListSerialization.propertyList(
+                from: data,
+                options: .mutableContainers,
+                format: nil
+              ) as? [String: String],
+              let value = plist[key] else { return "" }
+
+        return value
+    }
+
     var localized: String {
         return NSLocalizedString(self, tableName: "Localizable", value: self, comment: "")
     }
