1d056d49d63fbe7a473abd6fe4c7d8dd2c0b1c38
youseokhwan <youseokhwan15@gmail.com>
2022-09-22 00:56:59 +0900

    refactor: ExtensionsTests로 이름 수정

---
+++ Todolist/TodolistTests/ExtensionsTests.swift
@@ -0,0 +1,23 @@
+//
+//  ExtensionsTests.swift
+//  TodolistTests
+//
+//  Created by 유석환 on 2022/09/22.
+//
+
+import XCTest
+@testable import Todolist
+
+final class ExtensionsTests: XCTestCase {
+    override func setUpWithError() throws {
+
+    }
+
+    override func tearDownWithError() throws {
+
+    }
+
+    func testExample() throws {
+
+    }
+}
