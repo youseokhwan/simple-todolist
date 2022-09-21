6d84ed1712635827b1dbbd82269f29230fdffb59
youseokhwan <youseokhwan15@gmail.com>
2022-09-22 02:36:37 +0900

    feat: UILabelTests 테스트 코드 작성

--- Todolist/TodolistTests/ExtensionsTests.swift
+++ Todolist/TodolistTests/ExtensionsTests.swift
@@ -65,16 +65,52 @@ final class DateTests: XCTestCase {
 }
 
 final class UILabelTests: XCTestCase {
-    override func setUpWithError() throws {
+    var text: String!
+    var label: UILabel!
 
+    override func setUpWithError() throws {
+        text = "Hello World!"
+        label = UILabel()
     }
 
     override func tearDownWithError() throws {
-
+        text = nil
+        label = nil
     }
 
-    func testExample() throws {
+    func test_취소선적용() throws {
+        label.strikethrough(isActive: true, withText: text)
 
+        if let attributedText = label.attributedText,
+           let strikethroughAttribute = attributedText.attributes(at: 0, effectiveRange: nil)
+            .first(where: { $0.key == .strikethroughStyle }),
+           let strikethroughValue = strikethroughAttribute.value as? Int,
+           let foregroundColorAttribute = attributedText.attributes(at: 0, effectiveRange: nil)
+            .first(where: { $0.key == .foregroundColor }),
+           let foregroundColorValue = foregroundColorAttribute.value as? UIColor {
+            XCTAssertEqual(attributedText.string, text)
+            XCTAssertEqual(strikethroughValue, NSUnderlineStyle.single.rawValue)
+            XCTAssertEqual(foregroundColorValue, UIColor.systemGray)
+        } else {
+            XCTFail("XCTFail: variable is nil")
+        }
+    }
+
+    func test_취소선해제() throws {
+        label.strikethrough(isActive: false, withText: text)
+
+        if let attributedText = label.attributedText {
+            let strikethroughAttribute = attributedText.attributes(at: 0, effectiveRange: nil)
+                .first(where: { $0.key == .strikethroughStyle })
+            let foregroundColorAttribute = attributedText.attributes(at: 0, effectiveRange: nil)
+                .first(where: { $0.key == .foregroundColor })
+
+            XCTAssertEqual(attributedText.string, text)
+            XCTAssertNil(strikethroughAttribute)
+            XCTAssertNil(foregroundColorAttribute)
+        } else {
+            XCTFail("XCTFail: variable is nil")
+        }
     }
 }
 
