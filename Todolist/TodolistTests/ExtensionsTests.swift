e076223be1907366753e84097ebd885d747a689c
youseokhwan <youseokhwan15@gmail.com>
2022-09-22 01:38:08 +0900

    feat: StringTests 테스트 코드 작성

--- Todolist/TodolistTests/ExtensionsTests.swift
+++ Todolist/TodolistTests/ExtensionsTests.swift
@@ -9,16 +9,44 @@ import XCTest
 @testable import Todolist
 
 final class StringTests: XCTestCase {
+    var text: String!
+
     override func setUpWithError() throws {
 
     }
 
     override func tearDownWithError() throws {
-
+        text = nil
     }
 
-    func testExample() throws {
+    func test_앞뒤공백이없는경우() throws {
+        text = "Test"
+        XCTAssertEqual(text.trimmed, "Test")
+    }
 
+    func test_앞에공백이있는경우() throws {
+        text = " \nTest"
+        XCTAssertEqual(text.trimmed, "Test")
+    }
+
+    func test_뒤에공백이있는경우() throws {
+        text = "Test \n"
+        XCTAssertEqual(text.trimmed, "Test")
+    }
+
+    func test_앞뒤공백이있는경우() throws {
+        text = " \nTest \n"
+        XCTAssertEqual(text.trimmed, "Test")
+    }
+
+    func test_중간에공백이있는경우() throws {
+        text = "Te \nst"
+        XCTAssertEqual(text.trimmed, "Te \nst")
+    }
+
+    func test_앞뒤중간모두공백이있는경우() throws {
+        text = " \nTe \nst \n"
+        XCTAssertEqual(text.trimmed, "Te \nst")
     }
 }
 
