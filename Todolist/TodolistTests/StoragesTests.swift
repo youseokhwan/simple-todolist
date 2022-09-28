cf8de68bfdfde1be9da3a82af6b3a2b93f7f5bad
youseokhwan <youseokhwan15@gmail.com>
2022-09-28 16:40:25 +0900

    feat: dummyTasks 추가

--- Todolist/TodolistTests/StoragesTests.swift
+++ Todolist/TodolistTests/StoragesTests.swift
@@ -9,15 +9,32 @@ import XCTest
 import RealmSwift
 
 final class StoragesTests: XCTestCase {
+    var realm: Realm!
+
     override func setUpWithError() throws {
         Realm.Configuration.defaultConfiguration.inMemoryIdentifier = "StoragesTestsIdentifier"
+
+        let dummyTasks = [
+            Task(id: 0, title: "Lorem ipsum", isDone: false, isDaily: false, createdDate: Date()),
+            Task(id: 1, title: "dolor sit amet", isDone: false, isDaily: true, createdDate: Date()),
+            Task(id: 2, title: "consectetur", isDone: true, isDaily: false, createdDate: Date()),
+            Task(id: 3, title: "adipiscing elit", isDone: true, isDaily: true, createdDate: Date())
+        ]
+
+        realm = try Realm()
+        try realm.write {
+            dummyTasks.forEach { task in
+                realm.add(task)
+            }
+        }
     }
 
     override func tearDownWithError() throws {
-
+        realm = nil
     }
 
     func testExample() throws {
-        
+        let testArr = realm.objects(Task.self)
+        XCTAssertEqual(testArr.count, 4)
     }
 }
