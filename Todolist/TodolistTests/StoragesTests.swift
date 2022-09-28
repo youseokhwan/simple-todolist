f87ff29290ae457f66f40fa789277faff603e1b1
youseokhwan <youseokhwan15@gmail.com>
2022-09-28 15:51:21 +0900

    feat: StoragesTests.swift 파일 생성

---
+++ Todolist/TodolistTests/StoragesTests.swift
@@ -0,0 +1,22 @@
+//
+//  StoragesTests.swift
+//  TodolistTests
+//
+//  Created by 유석환 on 2022/09/28.
+//
+
+import XCTest
+
+final class StoragesTests: XCTestCase {
+    override func setUpWithError() throws {
+
+    }
+
+    override func tearDownWithError() throws {
+
+    }
+
+    func testExample() throws {
+
+    }
+}
