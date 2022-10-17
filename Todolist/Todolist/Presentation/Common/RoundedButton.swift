e0f4e777ca92c399cd319f09c5a5c8fe35f15a99
고재경 <folw159@gmail.com>
2022-10-18 00:49:23 +0900

    refactor: 생략가능한 UIFont 생략

--- Todolist/Todolist/Presentation/Common/RoundedButton.swift
+++ Todolist/Todolist/Presentation/Common/RoundedButton.swift
@@ -54,6 +54,6 @@ private extension RoundedButton {
     func configureViews() {
         contentHorizontalAlignment = .center
         setTitleColor(UIColor(.buttonText), for: .normal)
-        titleLabel?.font = UIFont.instance(.theCircleM, size: 18)
+        titleLabel?.font = .instance(.theCircleM, size: 18)
     }
 }
