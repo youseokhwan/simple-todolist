3722fef3940945bb47f2f0df3b69a43651bcacb0
youseokhwan <youseokhwan15@gmail.com>
2022-10-20 23:09:41 +0900

    feat: ViewModelsTests 파일 추가

---
+++ Todolist/TodolistTests/ViewModelsTests.swift
@@ -0,0 +1,22 @@
+//
+//  ViewModelsTests.swift
+//  TodolistTests
+//
+//  Created by 유석환 on 2022/10/20.
+//
+
+import XCTest
+
+final class ViewModelsTests: XCTestCase {
+    override func setUpWithError() throws {
+
+    }
+
+    override func tearDownWithError() throws {
+
+    }
+
+    func testExample() throws {
+
+    }
+}
